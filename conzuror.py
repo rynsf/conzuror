77cb9babdc2156a55b323f25643da3ded948daaa
rynsf <rynsf@users.noreply.github.com>
2024-01-18 19:57:37 +0530

    Add help

--- conzuror.py
+++ conzuror.py
@@ -202,6 +202,58 @@ def render(hz):
     pygame.display.flip()
     clock.tick(hz) 
 
+def help():
+    print(
+"""Conzuror
+You are a conjuror. You cast spells. With your spells you
+control the computer. You cast spells in a programming 
+language called spell, which is a lisp. While, you can
+also use keystrokes to  
+control the computer,                 ↑
+which is good for basic                W
+movements. You grasp the          ← A     D→
+true power of what the                 S
+computer is capable of                ↓   
+once you master the art 
+of casting spells. Lets have a basic look at how spell works.
+
+Spell
+Spell is your magic language. There are three kind of entities
+that can be written in a spell code. They are:
+* Numbers - like 7, or 2.
+* Atoms - are names and symbols.
+* Lists - A group of entities enclosed in parenthesis '( )'. 
+          Lists can contain any number of elements and even 
+          other lists.
+
+Function calls
+Spell is written with lists, which is its own data structure.
+The first elements of a list is considered to be a procedure,
+the other elements as arguments which are passed to the
+procedure.
+
+'+' is a function in spell which, you guess, adds. To add 
+numbers you would call '+' passing the numbers to be added 
+as arguments. Like this:
+
+ > (+ 5 1 3)
+ 9
+
+Spell have alot of these functions. They are the way by 
+which you task the computer to do something. You can also 
+define your own procedures. When you finally learn that
+you will become a true magician. You will be able to 
+command the computer at your will. But first, lets see 
+some more spells:
+
+Procedure calls can contain other procedure calls which 
+will be evaluated first and the value of which will be 
+passed to the outer call. 
+
+ > (+ 2 (- 5 3))
+ 4
+""")
+
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -219,17 +271,18 @@ spell.global_env["move-north"] = compy.moveNorth
 spell.global_env["move-south"] = compy.moveSouth
 spell.global_env["move-east"] = compy.moveEast
 spell.global_env["move-west"] = compy.moveWest
-spell.global_env["can-move-north"] = compy.canMoveNorth
-spell.global_env["can-move-south"] = compy.canMoveSouth
-spell.global_env["can-move-east"] = compy.canMoveEast
-spell.global_env["can-move-west"] = compy.canMoveWest
-spell.global_env["is-won"] = compy.isWon
-spell.global_env["can-move-right"] = compy.canMoveRight
+spell.global_env["can-move-north?"] = compy.canMoveNorth
+spell.global_env["can-move-south?"] = compy.canMoveSouth
+spell.global_env["can-move-east?"] = compy.canMoveEast
+spell.global_env["can-move-west?"] = compy.canMoveWest
+spell.global_env["is-won?"] = compy.isWon
+spell.global_env["can-move-right?"] = compy.canMoveRight
 spell.global_env["turn-right"] = compy.turnRight
-spell.global_env["can-move-left"] = compy.canMoveLeft
+spell.global_env["can-move-left?"] = compy.canMoveLeft
 spell.global_env["turn-left"] = compy.turnLeft
-spell.global_env["can-move-forward"] = compy.canMoveForward
+spell.global_env["can-move-forward?"] = compy.canMoveForward
 spell.global_env["move-forward"] = compy.moveForward
+spell.global_env["help"] = help
 
 print()
 print("Welcome to Conzuror!")
